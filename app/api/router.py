a668968601dff00de28e2ffc55f2ca68fe45c6c4
Christopher L. Lübbers <wsad@posteo.net>
2026-04-20 10:40:46 +0200

    added routes

--- app/api/router.py
+++ app/api/router.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 
+from app.api.routes.chat import router as chat_router
+from app.api.routes.compare import router as compare_router
 from app.api.routes.incidents import router as incidents_router
 from app.api.routes.prediction import router as prediction_router
 
 api_router = APIRouter()
 api_router.include_router(incidents_router)
 api_router.include_router(prediction_router)
+api_router.include_router(chat_router)
+api_router.include_router(compare_router)
